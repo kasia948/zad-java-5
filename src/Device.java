d57692b431f95eed9a0e591ef88cf39ff6c0ce1e
Kasia RN <rudyknowak@gmail.com>
2018-01-17 13:14:11 +0100

    wersja 1

---
+++ src/Device.java
@@ -0,0 +1,23 @@
+public class Device {
+    String code;
+    String producer;
+    String type;
+    double price;
+
+    public Device(String c, String p, String t, double pr) {
+        this.code = c;
+        this.producer = p;
+        this.type = t;
+        this.price = pr;
+    }
+
+    public Device(String code, String producer, String type) {
+        this.code = code;
+        this.producer = producer;
+        this.type = type;
+    }
+    void getInfo() {
+        System.out.println("Kod: "+code + ", producent: " + producer + ", typ: " + type + ", cena: " + price);
+    }
+}
+
